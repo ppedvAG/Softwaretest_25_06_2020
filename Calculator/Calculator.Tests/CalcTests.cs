caff937ce9712c9fa4a1703e13b0e5472fafa2f5
André <AndreR@ppedv.de>
2020-06-25 12:40:47 +0200

    Calculator Summen Test

---
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void Calc_Sum_3_and_4_results_7()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            int result = calc.Sum(3, 4);
+
+            //Assert
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void Calc_Sum_0_and_0_results_0()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            int result = calc.Sum(0, 0);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Calc_Sum_MAX_and_1__throws_OverflowException()
+        {
+            Calc calc = new Calc();
+
+            Assert.ThrowsException<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+        }
+
+        [TestMethod]
+        public void Calc_Sum_n6_and_9_results_3()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            int result = calc.Sum(-6, 9);
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+    }
+}
